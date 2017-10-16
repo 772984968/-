5e1a3437206fb23b7938999ebc6b1409755c4442
刘嘉华 <790928939@qq.com>
2017-10-16 15:27:15 +0800

    礼物日志表，礼物价格改为浮点

--- app/lib/nodes/UserMessage.php
+++ app/lib/nodes/UserMessage.php
@@ -1,7 +1,33 @@
 <?php
+namespace lib\nodes;
+use Yii;
 /**
- * Created by PhpStorm.
- * User: beck
- * Date: 2017/10/16
- * Time: 下午2:58
+ * Class UserMessage
+ * @package lib\nodes
+ * 系统给用户发送的信息存放在这里，
+ * 客户端登入后，获取这里的数据，告知客户，
+ * 信息，获取后消失
+ *
+ * 数据后进后出
  */
+class UserMessage
+{
+    const CACHE_NAME = 'atapp_lib_nodes_user_message_';
+    //加入一条信息
+    public static function send($user_id, $data)
+    {
+        return Yii::$app->redis->rpush(static::CACHE_NAME.$user_id, json_encode($data));
+    }
+
+    //取一条最原始的数据
+    public static function get($user_id)
+    {
+        $rst = Yii::$app->redis->lpop
+    }
+
+    //取该用户所有的数据
+    public static function getall($user_id)
+    {
+
+    }
+}
