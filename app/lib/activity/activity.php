e961975383b08eaf149f3273ee31de6adf25e6b6
unknown <790928939@qq.com>
2017-10-20 10:58:13 +0800

    加我的红包接口

--- app/lib/activity/activity.php
+++ app/lib/activity/activity.php
@@ -1,5 +1,3 @@
-
-
 <?php
 namespace lib\activity;
 use lib\models\ActivityDetailed;
@@ -128,6 +126,10 @@ class activity
 
     //取用户获得次数
     public static function getstatus($detailed) {
+        if(is_numeric($detailed)) {
+            $detailed = ActivityDetailed::findOne($detailed);
+        }
+        
         $rst = Yii::$app->redis->hget(static::USER_ACTIVITY_STATUS_CACHE.static::$userModel->iid, $detailed['a_id'].':'.$detailed['iid']);
         return $rst ?? '0';
     }
